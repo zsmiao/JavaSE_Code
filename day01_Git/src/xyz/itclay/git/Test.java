7c16dcbf4bcfcbeb6f65e5806a62858603a33a26
zsmiao <zsmiao2020@outlook.com>
2020-12-15 17:20:51 +0800

    第二次提交！

--- day01_Git/src/xyz/itclay/git/Test.java
+++ day01_Git/src/xyz/itclay/git/Test.java
@@ -6,5 +6,6 @@ package xyz.itclay.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("Hello Git!!!");
     }
 }
