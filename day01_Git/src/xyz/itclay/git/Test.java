63361a6677e480ca5a6544454a302fffdb73d8e3
zsmiao <zsmiao2020@outlook.com>
2020-12-15 17:18:57 +0800

    第一次提交！

---
+++ day01_Git/src/xyz/itclay/git/Test.java
@@ -0,0 +1,10 @@
+package xyz.itclay.git;
+
+/**
+ * @author Administrator
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
