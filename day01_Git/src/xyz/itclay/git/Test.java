4b6a8bc38345ab4d816edeffe107a89ccdf0d85c
zsmiao <zsmiao2020@outlook.com>
2020-12-15 18:52:05 +0800

    dev分支提交代码！

--- day01_Git/src/xyz/itclay/git/Test.java
+++ day01_Git/src/xyz/itclay/git/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
         System.out.println("Hello Git!!!");
+        System.out.println("哈哈哈哈，我是dev分支");
     }
 }
